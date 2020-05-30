1f5ff5e06ccb2993da3a09bbb4cba07e5e59a073
wangjingyi <wangjingyifighting@live.com>
2020-05-31 01:55:45 +0800

    类动态加载功能 实现只添加class 通过访问地址的参数构建实现热加载

---
+++ src/main/java/com/example/serious/demo/controller/DynamicClassLoadServ.java
@@ -0,0 +1,68 @@
+package com.example.serious.demo.controller;
+
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.startup.ClassLoaderFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Controller
+public class DynamicClassLoadServ extends ClassLoader{
+@GetMapping(value = "setClass")
+    protected String findClass(@RequestParam(value = "uri") String packageUri , @RequestParam(value = "name") String name) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        byte[] classData = getClassData(packageUri,name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        }
+        else {
+            Class myClass = null;
+            if(super.findLoadedClass(name) == null){
+                myClass = defineClass(name, classData, 0, classData.length);
+            }else{
+                System.out.println("===============动态类已经加载");
+                myClass = Class.forName(name);
+            }
+
+
+            for(Method m : myClass.getMethods()) {
+                System.out.println(m.getName());
+                if(m.getName().equals("index")){
+                    String result = (String)m.invoke(myClass.newInstance());
+                    return result;
+                }else{
+                    return "error";
+                }
+            }
+            return "final";
+
+        }
+    }
+
+    private byte[] getClassData(String packageUri , String className) {
+        String path = classNameToPath(packageUri,className);
+        try {
+            InputStream ins = new FileInputStream(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead = 0;
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String classNameToPath(String packageUri , String className) {
+        return packageUri + File.separatorChar
+                + className.replace('.', File.separatorChar) + ".class";
+    }
+}
---
+++ src/main/java/com/example/serious/demo/controller/TestDo.java
@@ -0,0 +1,7 @@
+package com.example.serious.demo.controller;
+
+public class TestDo {
+    public String index(){
+        return "zw.pdf";
+    }
+}

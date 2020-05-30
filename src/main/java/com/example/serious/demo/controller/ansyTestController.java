36e500b21e5886f763bd91dd40f54875b2f41dc5
wangjingyi <wangjingyifighting@live.com>
2020-05-31 01:57:38 +0800

    集成 mybatis 使用SqlSession

--- src/main/java/com/example/serious/demo/controller/ansyTestController.java
+++ src/main/java/com/example/serious/demo/controller/ansyTestController.java
@@ -1,11 +1,24 @@
 package com.example.serious.demo.controller;
 
+import com.example.serious.demo.mapper.UserMapper;
 import com.example.serious.demo.service.Impl.ansyTestService;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.mybatis.spring.SqlSessionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 @Controller
@@ -20,6 +33,21 @@ public class ansyTestController {
         Future<Integer> future1 =ansyTestService.methodB();
         long end = System.currentTimeMillis();
         return 1;
+    }
+    @Test
+    public void test1 () throws IOException {
+        int a = 13&17;
+        System.out.println("结果："+a);
+        Map map = new ConcurrentHashMap();
+        map.put("1231","321");
+        SqlSessionFactory sqlSessionFactory=null;
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        Reader reader = Resources.getResourceAsReader("application.properties");
+        sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession.getMapper(UserMapper.class);
+
     }
 
 }

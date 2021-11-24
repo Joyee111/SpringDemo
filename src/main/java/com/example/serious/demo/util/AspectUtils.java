67ace6fb3dd4bf95dae74fce634596b678ebd10b
wangjingyi <wangjingyifighting@live.com>
2021-11-24 21:35:17 +0800

    添加注解类

---
+++ src/main/java/com/example/serious/demo/util/AspectUtils.java
@@ -0,0 +1,10 @@
+package com.example.serious.demo.util;
+
+import java.lang.annotation.*;
+
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
+public @interface AspectUtils {
+
+}

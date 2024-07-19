f881ff3b5a2a9088a04453047e8de2a64497e50c
wangjingyi <wangjingyifighting@live.com>
2024-07-19 14:11:33 +0800

    Revert "测试回滚"
    This reverts commit 2646efa841b5f40a06d897412b5284ee41b55462.
    
    	modified:   src/main/java/com/example/serious/demo/controller/indexPage.java
    	modified:   src/main/java/com/example/serious/demo/mq/core/stream/MailProducer.java

--- src/main/java/com/example/serious/demo/mq/core/stream/MailProducer.java
+++ src/main/java/com/example/serious/demo/mq/core/stream/MailProducer.java
@@ -2,9 +2,7 @@ package com.example.serious.demo.mq.core.stream;
 
 import com.example.serious.demo.entity.FileEntity;
 import com.example.serious.demo.mq.core.RedisMQTemplate;
-import com.example.serious.demo.util.RedisDelayDeleteAspect;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -37,20 +35,7 @@ public class MailProducer {
         message.setFileName(mailAccountDO.getFileName());
         message.setId(mailAccountDO.getId());
         message.setHeaders(stringStringMap);
-        //testPrivate();
-        redisMQTemplate.send(message);
-    }
 
-    /**
-     *
-     * 其他类调用testPrivate()或redisMQTemplate.testPrivate()时，会触发切面
-     * 但sendMailSendMessage方法内调用testPrivate()时，不会触发切面
-     * 原因：aop基于MailProducer类实现了一个代理类MailProducerProxy，外部方法调用时是用MailProducerProxy调用，可以走到切面方法；内部调用时不会走MailProducerProxy，也就不会走到切面方法。
-     *
-     * 解决办法：将需要切面方法的部分新建成一个外部类MailProducerAspectService.class，并调用其方法
-     */
-    @RedisDelayDeleteAspect
-    public void testPrivate(){
-        log.info("私有方法");
+        redisMQTemplate.send(message);
     }
 }
